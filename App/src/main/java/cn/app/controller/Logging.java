846d6a0a98f7449c1590091fb0c08f26f67a5343
wang734941532 <73494132@qq.com>
2018-03-22 19:26:01 +0800

    app

---
+++ App/src/main/java/cn/app/controller/Logging.java
@@ -0,0 +1,25 @@
+package cn.app.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class Logging {
+
+	
+	@RequestMapping("/login")
+	public String login() {
+		return "enter";
+	}
+	
+	
+	@RequestMapping("/login-back")
+	public String login2() {
+		return "login-back";
+	}
+	
+	@RequestMapping("/login-developer")
+	public String login3() {
+		return "login-developer";
+	}
+}
